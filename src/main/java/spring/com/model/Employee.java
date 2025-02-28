85be4c6ae923a6e0072711af51d7af1c5baf987d
VivekChauhan6776 <vivek969800chauhan@gmail.com>
2025-02-28 21:17:37 +0530

    update

--- src/main/java/spring/com/model/Employee.java
+++ src/main/java/spring/com/model/Employee.java
@@ -1,15 +1,17 @@
 package spring.com.model;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-@Getter 
-@Setter
-@ToString
+//@Getter 
+@Setter//(value = AccessLevel.PRIVATE)
+@ToString(exclude = "name")
 @AllArgsConstructor
+//@Data
 
 public class Employee {
 
--- src/main/java/spring_core/App.java
+++ src/main/java/spring_core/App.java
@@ -1,10 +1,18 @@
 package spring_core;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import spring.com.model.Employee;
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		ApplicationContext ioc = new ClassPathXmlApplicationContext("applicationcontext.xml");
+				Employee bean = ioc.getBean(Employee.class, "emp");
+		        System.out.println(bean);
 	}
 
 }
+ 
